1ac5f28e4aa2450647cf32ee6f35896b44c044e7
Matthias Kargl <matthias.kargl@student.tugraz.at>
2019-03-27 15:29:33 +0100

    LANG_000: adapted data structure

---
+++ app/src/main/java/at/tugraz/ist/swe/teachingassistant/VocabularManger.java
@@ -0,0 +1,8 @@
+package at.tugraz.ist.swe.teachingassistant;
+import java.util.Vector;
+
+public class VocabularManger
+{
+    private Vector<Vocab> vocabs;
+    
+}
--- app/src/main/java/at/tugraz/ist/swe/teachingassistant/Vocabulary.java
+++
@@ -1,8 +0,0 @@
-package at.tugraz.ist.swe.teachingassistant;
-import java.util.Vector;
-public class Vocabulary
-{
-    private Vector<Vocab> vocabulary;
-
-    public Vocabulary(){}
-}
