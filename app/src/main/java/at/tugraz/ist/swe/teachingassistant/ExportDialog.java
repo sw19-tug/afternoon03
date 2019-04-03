9d526ef63ca34ab2bb5601a1e5e907285fa92f4e
Matthias Kargl <matthias.kargl@student.tugraz.at>
2019-04-03 11:06:12 +0200

    LANG_008 interface, fixed button id [KM, PD]

--- app/src/main/java/at/tugraz/ist/swe/teachingassistant/ExportDialog.java
+++ app/src/main/java/at/tugraz/ist/swe/teachingassistant/ExportDialog.java
@@ -6,28 +6,37 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ExportDialog extends AppCompatDialogFragment
 {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
-        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        final LayoutInflater inflater = requireActivity().getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_export, null);
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        builder.setView(inflater.inflate(R.layout.dialog_export, null))
+        builder.setView(dialogView)
             // Add action buttons
             .setPositiveButton(R.string.export, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int id) {
-                    // sign in the user ...
+                    EditText filename =
+                        ExportDialog.this.getDialog().findViewById(R.id.export_filename);
                 }
             })
+
             .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
+                public void onClick(DialogInterface dialog, int id)
+                {
+                    ExportDialog.this.getDialog().cancel();
                 }
             });
         return builder.create();
