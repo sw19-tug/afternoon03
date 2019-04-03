e902e144d4d08fe517d88f8fb50db2272f267970
Unknown <f.islitzer@get.systems>
2019-04-03 16:40:19 +0200

    LANG_008_A feat test parsing [KM,IF]

--- app/src/androidTest/java/at/tugraz/ist/swe/teachingassistant/LANG_008_ExportImportTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/teachingassistant/LANG_008_ExportImportTest.java
@@ -3,12 +3,19 @@ package at.tugraz.ist.swe.teachingassistant;
 
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
+//import com.google.gson.Gson;
 import com.google.gson.Gson;
+import com.google.gson.annotations.Expose;
+import com.google.gson.reflect.TypeToken;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.lang.reflect.Type;
+import java.util.Vector;
 
 import static org.junit.Assert.*;
 
@@ -19,24 +26,26 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class LANG_008_ExportImportTest
-{
+public class LANG_008_ExportImportTest {
     @Test
-    public void testExportPASS()
-    {
+    public void testExportPASS() {
         int ret = 1;
         Gson gson = new Gson();
         VocabularManager manager = VocabularManager.getInstance();
         Word w1 = new Word("Hallo", "DE");
         Word w2 = new Word("hello", "EN");
-        manager.addVocab(w1,w2);
-        gson.toJson(manager.getVocabs());
-
-        Log.w("JSON", gson.toString());
-
-
-        assertEquals(ret , 1);
-
+        manager.addVocab(w1, w2);
+        Type vocabVector = new TypeToken<Vector<Vocab>>() {
+        }.getType();
+        String jsonString = gson.toJson(manager.getVocabs());
+        Log.w("JSON before", gson.toJson(jsonString));
+        Vector<Vocab> vocabs = gson.fromJson(jsonString, vocabVector);
+        Log.e("Word test", vocabs.elementAt(0).getTranslation_table().elementAt(0).getText());
+       
+        boolean isEqual = vocabs.equals(manager.getVocabs());
+        assertEquals(true, isEqual);
+        //Log.w("JSON", gson.toJson(manager.getVocabs()));
+//        assertEquals(ret , 1);
     }
 
 }
