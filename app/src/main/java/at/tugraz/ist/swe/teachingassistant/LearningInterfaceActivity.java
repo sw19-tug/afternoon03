f5365a00b34db7c64badf52a310a3e2b42e13f02
saarasassi <sassisaara@gmail.com>
2019-04-08 18:00:28 +0200

    LANG_002_A Tests [SS, RG]

--- app/src/main/java/at/tugraz/ist/swe/teachingassistant/LearningInterfaceActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/teachingassistant/LearningInterfaceActivity.java
@@ -1,10 +1,14 @@
 package at.tugraz.ist.swe.teachingassistant;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+
 
 public class LearningInterfaceActivity extends AppCompatActivity {
     private String current_lang;
@@ -22,5 +26,17 @@ public class LearningInterfaceActivity extends AppCompatActivity {
         language_title.setText(words.get(current_position));
         TextView current_language = (TextView) findViewById(R.id.currentLanguage);
         current_language.setText(current_lang);
+    }
+
+    private void configureChangeLanguageButton() {
+        Button buttonChangeLanguage = (Button) findViewById(R.id.btn_changeLanguageInterface);
+        buttonChangeLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current_lang = current_lang == "en" ? "fi" : "en";
+                TextView language_title = (TextView) findViewById(R.id.currentLanguage);
+                language_title.setText(current_lang != "en" ? "Finnish" : "English");
+            }
+        });
     }
 }
--- app/src/main/java/at/tugraz/ist/swe/teachingassistant/LearningListActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/teachingassistant/LearningListActivity.java
@@ -1,16 +1,14 @@
 package at.tugraz.ist.swe.teachingassistant;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class LearningListActivity extends AppCompatActivity {
 
