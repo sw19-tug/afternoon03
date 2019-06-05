e0fb44c51ecea137f51fdcc00b52543cbe71ae3e
Daniel Pollhammer <pollhammer@student.tugraz.at>
2019-06-05 15:41:28 +0200

    LANG_018 added file [PD,RG]

---
+++ app/src/androidTest/java/at/tugraz/ist/swe/teachingassistant/LANG_018_EditEntity.java
@@ -0,0 +1,52 @@
+package at.tugraz.ist.swe.teachingassistant;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Vector;
+
+@RunWith(AndroidJUnit4.class)
+public class LANG_018_EditEntity
+{
+    @Rule
+    public ActivityTestRule<LearningListActivity> learingListActivity = new ActivityTestRule<LearningListActivity>(LearningListActivity.class, false, false);
+
+    @Before
+    public void setup() {
+        VocabularManager vocabularManager = VocabularManager.getInstance();
+        vocabularManager.clear();
+        Word word1 = new Word("first", "en");
+        Word word2 = new Word("first_trans", "fi");
+        Word word3 = new Word("second", "en");
+        Word word4 = new Word("second_trans", "fi");
+        Word word5 = new Word("third", "en");
+        Word word6 = new Word("third_trans", "fi");
+        vocabularManager.addVocab(word1, word2, null);
+        vocabularManager.addVocab(word3, word4, null);
+        vocabularManager.addVocab(word5, word6, null);
+    }
+
+    @Test void EditVocab()
+    {
+        VocabularManager manager = VocabularManager.getInstance();
+        manager.editVocabByWord("first","first_changed","trans_changed");
+        Vector<Vocab> vocabs = manager.getVocabs();
+        Assert.assertEquals(vocabs.elementAt(0).getTranslationByLanguage("en"), "first_changed");
+        Assert.assertEquals(vocabs.elementAt(0).getTranslationByLanguage("fi"), "trans_changed");
+
+    }
+    @Test void EditVocabNotInList()
+    {
+        VocabularManager manager = VocabularManager.getInstance();
+        manager.editVocabByWord("NOT_IN","first_changed","second_changed");
+        Vector<Vocab> vocabs = manager.getVocabs();
+        Assert.assertEquals(vocabs.elementAt(0).getTranslationByLanguage("en"), "first");
+        Assert.assertEquals(vocabs.elementAt(0).getTranslationByLanguage("fi"), "first_trans");
+    }
+}
