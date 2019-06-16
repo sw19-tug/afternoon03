32a6958bae73441e2b82c4970bae9eba5149cc03
Hugh H Sheahan <hugh.sheahan@uvm.edu>
2019-06-16 18:03:58 +0200

    LANG_006 Testing outline [SC,SD]

---
+++ app/src/androidTest/java/at/tugraz/ist/swe/teachingassistant/LANG_006_AdvancedTesting.java
@@ -0,0 +1,29 @@
+package at.tugraz.ist.swe.teachingassistant;
+
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class LANG_006_AdvancedTesting {
+
+    //TODO:I will do these tests -SC
+    @Test
+    public void checkSelectTestInterface() throws Exception {
+
+
+
+
+    }
+
+    @Test
+    public void checkAdvancedTestSelectorInterface() throws Exception {
+
+
+
+
+    }
+}
