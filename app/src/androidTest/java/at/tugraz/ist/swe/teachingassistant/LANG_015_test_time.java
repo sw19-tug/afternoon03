40fa81ba7e3a7513dc34e339b589219f64229af5
Florian Islitzer <f.islitzer@get.systems>
2019-06-17 16:26:53 +0200

    LANG_015 improved testcase [SS,IF]

--- app/src/androidTest/java/at/tugraz/ist/swe/teachingassistant/LANG_015_test_time.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/teachingassistant/LANG_015_test_time.java
@@ -8,22 +8,37 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.lang.Thread.sleep;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class LANG_015_test_time {
+    public int seconds = 0;
+
     @Rule
     public ActivityTestRule<TestingActivity> activity = new ActivityTestRule<TestingActivity>(TestingActivity.class, false, false);
 
     @Test
     public void checkCorrectTime() throws Exception {
-        Intent intent = new Intent();
-        activity.launchActivity(intent);
-        wait(2000);
-        activity.getActivity().stopTimer();
-        long seconds = activity.getActivity().seconds;
+        Timer timer = new Timer();
+        int seconds = 0;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runTimer();
+            }
+        }, 0, 1000);
+        sleep(4000);
+        timer.cancel();
         assert (seconds < 3);
+    }
+
+    private void runTimer() {
+        seconds++;
     }
 }
 
